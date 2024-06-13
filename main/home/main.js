97d9c6c3c568024714813b33777e6751faa35618
pingmommy <mcar8764@naver.com>
2024-06-13 15:18:09 +0900

    main 슬라이드추가, exclusive,preshow 공간조정

--- main/home/main.js
+++ main/home/main.js
@@ -8,6 +8,32 @@ const swiper = new Swiper(".mySwiper", {
   },
 });
 
+const swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+});
+
 const nav = document.querySelector("#nav");
 
 window.addEventListener("scroll", () => {
